54f43da542519bc86eb8a98419af34b32ec36869
brentli1 <brent.jlinville@gmail.com>
2017-06-23 21:46:18 -0500

    add category controller and associated tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624014824) do
+ActiveRecord::Schema.define(version: 20170624015939) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 20170624014824) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
